b32e93bffea0dbea9c941614d52a3f78aebd4870
baranacikgoz <baran-acikgoz@outlook.com>
2022-08-23 13:46:42 +0300

    implemented 'getAll' in controller

---
+++ src/WebAPI/ProductApp.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApp.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepositoryAsync _productRepository;
+
+        public ProductController(IProductRepositoryAsync productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allList = await _productRepository.GetAllAsync();
+            var result = allList.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name,
+            });
+
+            return Ok(result);
+        }
+    }
+}
